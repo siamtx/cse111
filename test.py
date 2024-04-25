a64e2bf49d140d561d173760dccf169c0f394e49
siamtx <siamtx@yahoo.com>
2024-04-24 22:15:39 -0400

    corrected tire_vloume.py and deleted test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-un = 1
-du = 2
-tois = 3
-
-set =+ un + du
-
-print(set)
--- tire_volume.py
+++ tire_volume.py
@@ -16,6 +16,5 @@ t = (z * w**2 * ar *(w * ar + 2540 * d))/ 10000000000
 # Output the results
 print(f"The approximate volume is {t:.2f} liters.")
 
-print(" the github has too many files in it.")
 
 
