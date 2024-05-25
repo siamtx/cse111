eb6e11a37e1e7ddb47ea2da83ef634ee717d4021
siamtx <siamtx@yahoo.com>
2024-05-25 11:34:52 -0400

    Week 5 programs completed.

--- test_water_flow.py
+++ test_water_flow.py
@@ -13,19 +13,19 @@ def test_water_column_height():
 
 def test_pressure_gain_from_water_height():
 
-    assert pressure_gain_from_water_height(0, 0) == 0.001
-    assert pressure_gain_from_water_height(30.2, 295.628) == 0.001
-    assert pressure_gain_from_water_height(50, 489.450) == 0.001
+    assert pressure_gain_from_water_height(0) == approx(0, abs= 0.001)
+    assert pressure_gain_from_water_height(30.2) == approx(295.628, abs= 0.001)
+    assert pressure_gain_from_water_height(50) == approx(489.450, abs= 0.001)
 
 
-def pressure_loss_from_pipe():
-    assert pressure_loss_from_pipe(0.048692, 0, 0.018, 1.75) == 0
-    assert pressure_loss_from_pipe(0.048692, 200, 0, 1.75) == 0
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 0) == 0
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == -113.008
-    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == -100.462
-    assert pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == -61.576
-    assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == -110.884
+def test_pressure_loss_from_pipe():
+    assert pressure_loss_from_pipe(0.048692, 0, 0.018, 1.75) == approx(0, abs= 0.001)
+    assert pressure_loss_from_pipe(0.048692, 200, 0, 1.75) == approx(0, abs= 0.001)
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 0) == approx(0, abs= 0.001)
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.75) == approx(-113.008, abs= 0.001) 
+    assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, abs= 0.001)
+    assert pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == approx(-61.576, abs= 0.001)
+    assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, abs= 0.001)
 
 """
                                             Expected     approx
--- water_flow.py
+++ water_flow.py
@@ -1,38 +1,41 @@
 
-
+# Calculate the water column height with this formula
+# h = t + (3*w/4)
+# h is height of the water column
+# t is the height of the tower
+# w is the height of the walls of the tank that is on 
+# top of the tower
 def water_column_height(tower_height, tank_height):
+    height = tower_height + 3*tank_height/4
 
-    """  
-    h = t + 3w/4
-    where
-    h is height of the water column
-    t is the height of the tower
-    w is the height of the walls of the tank that is on top of the tower
-    """
+    return height
 
-def pressure_gain_from_water_height(height):
-    """
-    P = ρgh/1000
+ 
+# Calculates pressure gain useing the following info
+#     P = ρgh/1000
+# P is the pressure in kilopascals
+# ρ is the density of water (998.2 kilogram / meter3)
+# g is the acceleration from Earths gravity (9.80665 meter / second2)
+# h is the height of the water column in meters
+def pressure_gain_from_water_height(height):    
+    d = 998.2
+    g = 9.80665
 
-    where
-    P is the pressure in kilopascals
-    ρ is the density of water (998.2 kilogram / meter3)
-    g is the acceleration from Earths gravity (9.80665 meter / second2)
-    h is the height of the water column in meters
-    """
+    pressure = d*g*height/1000
 
-
+    return pressure
+ 
+# Calculates pressure loss
+# P is the lost pressure in kilopascals
+# f is the pipe’s friction factor
+# L is the length of the pipe in meters
+# ρ is the density of water (998.2 kilogram / meter3)
+# v is the velocity of the water flowing through the pipe in meters / second
+# d is the diameter of the pipe in meters
 def pressure_loss_from_pipe(pipe_diameter,
-        pipe_length, friction_factor, fluid_velocity):
-    
-    """
-    P = -(f*L*p*v**2)/2000*d
+    pipe_length, friction_factor, fluid_velocity):
 
-    where
-    P is the lost pressure in kilopascals
-    f is the pipe’s friction factor
-    L is the length of the pipe in meters
-    ρ is the density of water (998.2 kilogram / meter3)
-    v is the velocity of the water flowing through the pipe in meters / second
-    d is the diameter of the pipe in meters
-    """
+    pressure_loss = (-friction_factor * pipe_length * 998.2 * fluid_velocity**2) / (2000 * pipe_diameter)
+
+    return pressure_loss
+    
