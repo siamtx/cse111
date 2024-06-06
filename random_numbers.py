dc639a8b726cc18f394cc7194a37f23ec3c2b459
siamtx <siamtx@yahoo.com>
2024-06-06 06:45:58 -0400

    deleted the pycache files.

---
+++ random_numbers.py
@@ -0,0 +1,26 @@
+import random
+import math
+
+
+def main():
+    numbers = [16.2, 75.1, 52.3]
+    print(numbers)
+
+    ranNum = append_random_numbers(num)
+    print(numbers)
+
+    ranNum1 = append_random_numbers(num1, num2, num3)
+    print(numbers)
+
+
+def append_random_numbers(numbers_list, quantity):
+    quantity = random.uniform()
+    psuedo = round(quantity)
+    numbers_list.apppend(psuedo)
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
