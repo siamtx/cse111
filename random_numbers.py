32594024c652fa6f644036b3f22cbd2540284b6b
siamtx <siamtx@yahoo.com>
2024-06-09 01:01:07 -0400

    updating with chemesty and test chemestry

--- random_numbers.py
+++ random_numbers.py
@@ -1,26 +1,61 @@
 import random
-import math
-
 
 def main():
     numbers = [16.2, 75.1, 52.3]
-    print(numbers)
+    print(f"numbers {numbers}")
 
-    ranNum = append_random_numbers(num)
-    print(numbers)
+    append_random_numbers(numbers)
+    print(f"numbers {numbers}")
 
-    ranNum1 = append_random_numbers(num1, num2, num3)
-    print(numbers)
+    append_random_numbers(numbers, 3)
+    print(f"numbers {numbers}")
+
+    words = []
+
+    append_random_words(words)
+    print(f"words {words}")
+
+    append_random_words(words, 5)
+    print(f"words {words}")
+
+    sentences = []
+
+    append_random_sentences(sentences)
+    print(f"sentences {sentences}")
+
+    append_random_sentences(sentences, 5)
+    print(f"sentences {sentences}")
 
 
-def append_random_numbers(numbers_list, quantity):
-    quantity = random.uniform()
-    psuedo = round(quantity)
-    numbers_list.apppend(psuedo)
+def append_random_numbers(numbers_list, quantity = 1):
+    for _ in range(quantity):
+        random_number = random.uniform(0, 100)
+        rounded = round(random_number, 1)
+        numbers_list.append(rounded)
+
+
+def append_random_words(words_list, quantity = 1):
+
+    candidates = ["bird", "red", "blue", "whale", "car", "tire",
+                  "join", "laugh", "love", "sleep", "smile", "speak",
+        "sunshine", "toothbrush", "tree", "truth", "walk", "water"]
+
+    for _ in range(quantity):
+         word = random.choice(candidates)
+         words_list.append(word)
+
+def append_random_sentences(sentences_list, quantity = 1):
+    candidates = ['The brown fox ran.', 'The yellow cat meowed.', 'The blue bird flew away.'
+                   'Cats and dogs fight.', 'Cheese is not a food.', 'the moon is not green.',
+                   'Children can be polite.', 'Programming can be fun sometimes.', 
+                   'BYUI education systems are challenging.', 'The sun will rise tomorrow.', 
+                   'the weather is really nice outside.']
+    for _ in range(quantity):
+         sentence = random.choice(candidates)
+         sentences_list.append(sentence)
 
 
 
 
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
         main()
